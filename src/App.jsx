a9348a5e47fa45174010604f6d8549ec596740f8
Alexandre Lemes de Santana Filho <126908528+AlexandreSantanaa@users.noreply.github.com>
2023-12-16 05:19:59 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numero, setNumero] = useState(0)
 
+  function aumentar() {
+    setNumero(numero + 1);
+  }
+
+  function diminuir() {
+    setNumero(numero - 1);
+  }
+
+  function resetar() {
+    setNumero(numero - numero);
+  }
   return (
-    <>
+    <div id="contador">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <button id="menos" className='botao-contar' onClick={diminuir}>-</button>
+        </div>
+
+        <span id="valor" >{numero}</span>
+
+        <div>
+          <button id="mais" className='botao-contar' onClick={aumentar}>+</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <button id="resetar" onClick={resetar}>resetar</button>
+
+    </div>
+
   )
 }
 
